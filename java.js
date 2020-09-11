82aedf30a5767ee1646b58fc9c8db4cdce00569c
andbret <and.bretnall@gmail.com>
2020-09-11 16:50:30 -0400

    fully functional

--- java.js
+++ java.js
@@ -1,3 +1,16 @@
+
+window.onload = document.getElementById("test9").innerHTML = localStorage.getItem("a");
+window.onload = document.getElementById("test10").innerHTML = localStorage.getItem("b");
+window.onload = document.getElementById("test11").innerHTML = localStorage.getItem("c");
+window.onload = document.getElementById("test12").innerHTML = localStorage.getItem("d");
+window.onload = document.getElementById("test13").innerHTML = localStorage.getItem("e");
+window.onload = document.getElementById("test14").innerHTML = localStorage.getItem("f");
+window.onload = document.getElementById("test15").innerHTML = localStorage.getItem("g");
+window.onload = document.getElementById("test16").innerHTML = localStorage.getItem("h");
+window.onload = document.getElementById("test17").innerHTML = localStorage.getItem("j");
+
+  
+
 console.log(moment())
 function update() {
     // $('#clock').html(moment().format('D. MMMM YYYY H:mm:ss'));
@@ -18,11 +31,17 @@ for(var i=0; i<elements.length; i++) {
     var tryy = parseInt(names);
 
     if (tryy === test){
-        elements[i].style.backgroundColor = "lightblue"; 
+        elements[i].style.backgroundColor = "red";
+        elements[i].style.color = "white"; 
+    }
+    else{
+        elements[i].style.backgroundColor = "lightgrey";
     }
     if (tryy > test){
-        elements[i].style.backgroundColor = "blue"; 
+        elements[i].style.backgroundColor = "lightgreen"; 
     }
+
+
 
     console.log(names);
     console.log(elements[i]);
@@ -32,24 +51,125 @@ for(var i=0; i<elements.length; i++) {
 
 
 
-var formInput = document.querySelector("#test9");
-$("#submit").click(function(){
+
+$("#submit9").click(function(){
+    var formInput = document.querySelector("#test9");
     event.preventDefault();
     console.log("Form was submitted!");
-    
-    var reminderText = formInput.value;
+      var reminderText = formInput.value;
 
     console.log(reminderText);
-    document.getElementById("test9").placeholder = reminderText;
-    localStorage.setItem("toost", reminderText);
-    var ihope = localStorage.getItem("toost");
-    document.getElementById("test10").placeholder = ihope;
-       
-      
-
+    localStorage.setItem("a", reminderText);
    
-      
-  });
+    document.getElementById("test9").innerHTML = localStorage.getItem("a");
+    localStorage.setItem("a", reminderText);
+    console.log(localStorage.getItem("a"));
+});
+$("#submit10").click(function(){
+    var formInput = document.querySelector("#test10");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("b", reminderText);
+   
+    document.getElementById("test10").innerHTML = localStorage.getItem("b");
+    localStorage.setItem("b", reminderText);
+    console.log(localStorage.getItem("b"));
+});
+$("#submit11").click(function(){
+    var formInput = document.querySelector("#test11");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("c", reminderText);
+   
+    document.getElementById("test11").innerHTML = localStorage.getItem("c");
+    localStorage.setItem("c", reminderText);
+    console.log(localStorage.getItem("c"));
+});
+$("#submit12").click(function(){
+    var formInput = document.querySelector("#test12");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("d", reminderText);
+   
+    document.getElementById("test12").innerHTML = localStorage.getItem("d");
+    localStorage.setItem("d", reminderText);
+    console.log(localStorage.getItem("d"));
+});
+$("#submit13").click(function(){
+    var formInput = document.querySelector("#test13");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("e", reminderText);
+   
+    document.getElementById("test13").innerHTML = localStorage.getItem("e");
+    localStorage.setItem("e", reminderText);
+    console.log(localStorage.getItem("e"));
+});
+$("#submit14").click(function(){
+    var formInput = document.querySelector("#test14");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("f", reminderText);
+   
+    document.getElementById("test14").innerHTML = localStorage.getItem("f");
+    localStorage.setItem("f", reminderText);
+    console.log(localStorage.getItem("f"));
+});
+$("#submit15").click(function(){
+    var formInput = document.querySelector("#test15");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("g", reminderText);
+   
+    document.getElementById("test15").innerHTML = localStorage.getItem("g");
+    localStorage.setItem("g", reminderText);
+    console.log(localStorage.getItem("g"));
+});
+$("#submit16").click(function(){
+    var formInput = document.querySelector("#test16");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("h", reminderText);
+   
+    document.getElementById("test16").innerHTML = localStorage.getItem("h");
+    localStorage.setItem("h", reminderText);
+    console.log(localStorage.getItem("h"));
+});
+$("#submit17").click(function(){
+    var formInput = document.querySelector("#test17");
+    event.preventDefault();
+    console.log("Form was submitted!");
+      var reminderText = formInput.value;
+
+    console.log(reminderText);
+    localStorage.setItem("j", reminderText);
+   
+    document.getElementById("test17").innerHTML = localStorage.getItem("j");
+    localStorage.setItem("j", reminderText);
+    console.log(localStorage.getItem("j"));
+});
+
 
 
 
